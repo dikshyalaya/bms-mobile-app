4161764a2040bb9a028f05307dd7bc9e51ebe916
Anil Kandel <kandelanil24@gmail.com>
2024-06-04 10:53:01 +0545

    Manager Dashboard size fixed in Tab

--- lib/features/manager_dashboard/home/widget/manager_dashboard_home.dart
+++ lib/features/manager_dashboard/home/widget/manager_dashboard_home.dart
@@ -1,7 +1,5 @@
-
 import 'package:beacon_flutter/common/widgets/builder/ifbuilder.dart';
 import 'package:beacon_flutter/common/widgets/scaffold_background_wrapper.dart';
-import 'package:beacon_flutter/features/auth/data/bms_user_model.dart';
 import 'package:beacon_flutter/features/auth/domain/auth_provider.dart';
 import 'package:beacon_flutter/features/manager_dashboard/home/domain/manager_permission_provider.dart';
 import 'package:beacon_flutter/features/manager_dashboard/home/widget/manager_dashboard_grid.dart';
@@ -10,25 +8,25 @@ import 'package:provider/provider.dart';
 
 import '../../../../common/widgets/beacon_app_bar.dart';
 
-class ManagerDashBoardScreen extends StatelessWidget {
+class ManagerDashBoardScreen extends StatefulWidget {
   const ManagerDashBoardScreen({Key? key}) : super(key: key);
 
   @override
+  State<ManagerDashBoardScreen> createState() => _ManagerDashBoardScreenState();
+}
+
+class _ManagerDashBoardScreenState extends State<ManagerDashBoardScreen> {
+  @override
   Widget build(BuildContext context) {
-    // final authProvider = Provider.of<AuthProvider>(context, listen: false)
-    //   ..getUserDetail();
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+
     return ScaffoldBackGroundWrapper(
+      appBar: BeaconAppBar(
+        title:
+            "${authProvider.bmsUserModel?.empFirstName} ${authProvider.bmsUserModel?.empLastName}",
+      ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: PreferredSize(
-            preferredSize: const Size(double.infinity, 68),
-            child: Selector<AuthProvider, BmsUserModel?>(
-              selector: (context, provider) => provider.bmsUserModel,
-              builder: (context, bmsUserModel, child) => BeaconAppBar(
-                title:
-                    "${bmsUserModel?.empFirstName} ${bmsUserModel?.empLastName}",
-              ),
-            )),
         body: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
